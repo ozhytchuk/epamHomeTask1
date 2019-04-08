ef796a18052b13b48f28aa5654f832b1052c701e
Andriy Ozhytchuk <ozhytchuk@gmail.com>
2019-04-08 15:37:09 +0300

    description bonus task

--- src/functions.php
+++ src/functions.php
@@ -86,7 +86,24 @@ function sumNumbers($num)
     return ($firstNum === $secondNum);
 }
 
-// BONUS TASK #1
+/*
+    Compare two strings by comparing the sum of their values (ASCII character code).
+    For comparing treat all letters as UpperCase.
+
+    Null-Strings should be treated as if they are empty strings.
+    If the string contains other characters than letters, treat the whole string as it would be empty.
+
+    Examples:
+    "AD","BC" -> equal
+    "AD","DD" -> not equal
+    "gf","FG" -> equal
+    "zz1","" -> equal
+    "ZzZz", "ffPFF" -> equal
+    "kl", "lz" -> not equal
+    null, "" -> equal
+
+    Your method should return true, if the strings are equal and false if they are not equal.
+*/
 function asciiCode($firstString, $secondString)
 {
     $splitFirstString = str_split($firstString);
